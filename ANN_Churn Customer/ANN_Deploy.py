84b9fa68456028e22ee2bfc6cc058c58c15cb14b
Sayantan Sarkar <deepsarkar0@gmail.com>
2024-09-11 19:18:50 +0530

    Churn Customer Deploy Streamlit

---
+++ ANN_Churn Customer/ANN_Deploy.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import tensorflow as tf, keras
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, MinMaxScaler
+import pickle
+
+model = tf.keras.models.load_model('model.h5')
+
+with open('label_encode.pkl','rb') as file:
+    label_encode = pickle.load(file)
+
+with open('onehot_encode.pkl','rb') as file:
+    onehot_encode = pickle.load(file)
+    
+with open('scale.pkl','rb') as file:
+    scale = pickle.load(file)    
+    
+st.title('Customer Churn Prediction')
+geography = st.selectbox('Geography',onehot_encode.categories_[0])
+gender = st.selectbox('Gender',label_encode.classes_)
+age = st.slider('Age',18,100)
+balance = st.number_input('Balance')
+credit_score = st.number_input('Credit Score')
+estimated_salary = st.number_input('Estimated Salary')
+tenure = st.slider('Tenure',1,36)
+num_of_products = st.slider('Number Of Products',1,8)
+has_cr_card = st.selectbox('Has Credit Card',[0,1])
+is_active_member = st.selectbox('Is Active Member',[0,1])
+
+input_data = [{
+    'CreditScore':credit_score,
+    'Gender':label_encode.transform([gender])[0],
+    'Age':age,
+    'Tenure':tenure,
+    'Balance':balance,
+    'NumOfProducts':num_of_products,
+    'HasCrCard':has_cr_card,
+    'IsActiveMember':is_active_member,
+    'EstimatedSalary':estimated_salary,
+}]
+
+input_data = pd.DataFrame(input_data) # Converting to DataFrame
+input_data[['CreditScore','Age','Balance','EstimatedSalary','Tenure']] = scale.transform(input_data[['CreditScore','Age','Balance','EstimatedSalary','Tenure']]) # Scaling the data
+geo_encode = onehot_encode.transform([[geography]]) # One Hot encoding the data
+input_data = pd.concat([input_data.reset_index(drop=True),geo_encode],axis=1) # Concatenating the whole data
+print(input_data)
+prediction = model.predict(input_data) # Predicting the model
+
+if prediction[0][0] > 0.5:
+    st.write('The customer is likely to Churn')
+else:
+    st.write('The customer will Stay')
